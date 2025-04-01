0793f7394441b63ce91d3e02fc7895edded5fff9
ErnestoGarSa <ernest0_10@hotmail.com>
2025-04-01 15:10:22 -0600

    Se crea la pagina de not-found, se hace una base de datos con sqlite3 y el grid de las comidas

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const not_found = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default not_found;
