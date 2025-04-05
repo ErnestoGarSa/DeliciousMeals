f8a8541da04d7d9b541423220fdba2740de5f7e7
ErnestoGarSa <ernest0_10@hotmail.com>
2025-04-04 18:10:43 -0600

    Se crea la pagina de las comidas en especifico y el componente para subir una imagen

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,10 @@
+const notFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or meal data.</p>
+    </main>
+  );
+};
+
+export default notFound;
